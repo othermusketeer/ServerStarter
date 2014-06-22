811702907734092ad58c1b3dcaf4c802bf260115
othermusketeer <othermusketeer@users.noreply.github.com>
2014-06-22 04:13:30 -0400

    OPEN - task 14: Cleanup setup.py http://github.com/othermusketeer/ServerStarter/issues/issue/14

--- setup.py
+++ setup.py
@@ -5,41 +5,47 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-	
+    
 project_version = '0.1'
 
 here = os.path.abspath(os.path.dirname(__file__))
+
 try:
-	README = open(os.path.join(here, 'README.rst')).read()
+    README = open(os.path.join(here, 'README.rst')).read()
 except:
-	README = ""
+    README = ""
+if README=="":
+    try:
+        README = open(os.path.join(here, 'README.md')).read()
+    except:
+        README = ""
+        
 try:
-	CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 except:
-	CHANGES = ""
+    CHANGES = ""
 
 requires = []
 #requires=['pyramid>=1.3', 'pyramid_jinja2', 'wtforms', 'webhelpers']
 
 config = {
-    'description': 'My Project',
-	'long_description': README + '\n\n' +  CHANGES,
+    'description': 'Python package, script, and resources for setting up new linux servers.',
+    'long_description': README + '\n\n' +  CHANGES,
     'author': 'D\'Artagnan Palmer',
-	'author_email': 'dpalmer@othermusketeer.com',
-    'url': 'URL to get it at.',
-	'license' : 'MIT License',
-    'download_url': 'Where to download it.',
+    'author_email': 'dpalmer@othermusketeer.com',
+    'url': 'https://github.com/othermusketeer/ServerStarter',
+    'license' : 'MIT License',
+    'download_url': 'https://github.com/othermusketeer/ServerStarter/releases',
     'version': project_version,
-    'install_requires': ['nose'],
     'packages': find_packages(),
-    'scripts': [],
-	'install_requires': requires,
-	'tests_require': requires,
-	'platforms': 'any',
-	'test_suite': 'NAME.test.NAME_tests',
-    'name': 'projectname',
-	'zip_safe': False,
-	'namespace_packages': ['othermusketeer']
+    'scripts': ['bin/NewBox.py'],
+    'install_requires': requires,
+    'tests_require': requires,
+    'platforms': 'any',
+    'test_suite': 'ServerStarter.test.serverstarter_tests',
+    'name': 'ServerStarter',
+    'zip_safe': False,
+    'namespace_packages': ['othermusketeer']
 }
 
 classifiers = [
@@ -54,13 +60,13 @@ classifiers = [
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
         ]
-		
+        
 classifiers = [
         'Programming Language :: Python',
         'Development Status :: 4 - Beta',
         'Natural Language :: English',
         'Operating System :: OS Independent'
-	]
+    ]
 config['classifiers'] = classifiers
 
 setup(**config)
